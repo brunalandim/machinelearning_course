1c0533a89ef5d4158fd78ea6403568bb635bea59
Bruna Landim <56938713+linnep@users.noreply.github.com>
2019-11-21 18:08:53 -0300

    breast_cancer_simples
    It's about one metod a data set studied in class

---
+++ breast_cancer_simples.py
@@ -0,0 +1,7 @@
+#breast_cancer
+import pandas as pd 
+previsores = pd.read_csv('entradas-breast.csv')
+classe = pd.read_csv('saidas-breast.csv')
+
+from sklearn.model_selection import train_test_split
+previsores_treinamento, previsores_teste, classe_treinamento, classe_teste = train_test_split(previsores,classe, test_size=0.25)
