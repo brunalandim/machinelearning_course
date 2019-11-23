0ba7e0c157d6d627800844ddce01ecde3ae803a0
Bruna Landim <56938713+linnep@users.noreply.github.com>
2019-11-23 16:46:44 -0300

    neural_structure
    creating neural structure. Early training predictions.

--- breast_cancer_simples.py
+++ breast_cancer_simples.py
@@ -5,3 +5,14 @@ classe = pd.read_csv('saidas-breast.csv')
 
 from sklearn.model_selection import train_test_split
 previsores_treinamento, previsores_teste, classe_treinamento, classe_teste = train_test_split(previsores,classe, test_size=0.25)
+
+import keras
+from keras.models import Sequential
+from keras.layers import Dense 
+
+classificador = Sequential()
+classificador.add (Dense(units= 16, activation = 'relu', kernel_initializer = 'random_uniform', input_dim = 30))
+classificador.add (Dense(units = 1, activation = 'sigmoid'))
+
+classificador.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['binary_accuracy'] )
+classificador.fit(previsores_treinamento, classe_treinamento, batch_size = 10, epochs = 100)
